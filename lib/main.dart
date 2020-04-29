f0e301f1e6aaaae88fb81190da8f5294e3a79065
Jeet Undaviya <jeet.undaviya20017@gmail.com>
2020-04-29 06:17:31 +0530

    All Database backend is complited Now you Just have to update the Reports page and manage the frontend for the same for streambuilder and also have a look to dashboard page as it refreshes but it is not updating the information of the user i always have to rotate screen and then it gets updated.....

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+//import 'package:flashlabs/dashboard.dart';
+import 'package:flashlabs/screens/lab1.dart';
+import 'package:flashlabs/screens/lab2.dart';
+import 'package:flashlabs/screens/lab3.dart';
+import 'package:flashlabs/screens/reportscreen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flashlabs/screens/loginpage.dart';
+import 'package:flashlabs/screens/dashboard.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
+  //    home: Report(),
     );
   }
 }
